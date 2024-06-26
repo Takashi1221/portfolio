a2635ef74d4c01b6fdfffccaf7cfbb7675414e37
t-mio <takasshhi21@gmail.com>
2024-06-26 16:07:47 +0200

    AWS-SDK

--- components/DataFetcher.tsx
+++ components/DataFetcher.tsx
@@ -34,8 +34,8 @@ const DataFetcher: React.FC<DataFetcherProps> = ({ children }) => {
             // AWS SDKの設定
             AWS.config.update({
                 region: 'eu-central-1',
-                accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
-                secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
+                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
             });
 
             const s3 = new AWS.S3();
