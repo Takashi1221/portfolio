6aae9962c213eb42a2e3977343790bc54a48d199
t-mio <takasshhi21@gmail.com>
2024-06-26 16:18:04 +0200

    new secret

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    // 他の設定があればここに追加
-    images: {
-        unoptimized: true,
-      },
+  output: 'export',
+  // 他の設定があればここに追加
+  images: {
+    unoptimized: true,
+  },
+  env: {
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 };
 
 export default nextConfig;
